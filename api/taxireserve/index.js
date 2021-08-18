03cc100a47680863b1b8ad9227496b7387d58d77
Kazuyuki Miyake <k-miyake@zengeeks.io>
2021-08-18 19:36:11 +0900

    TaxiReserve の Function を async 化する (#7)
    * taxireserve を async 化
    
    * context.done() を削除

--- api/taxireserve/index.js
+++ api/taxireserve/index.js
@@ -1,59 +1,43 @@
-const http = require('https');
+const fetch = require('node-fetch');
 const uuid = require('uuid');
 
-module.exports = function (context, req) {
+module.exports = async function (context, req) {
 
-	let userId = "";
-	const json_mess = "";
+	// LINE Login の id_token 検証にアクセスするパラメータを設定
+	const send_url = "https://api.line.me/oauth2/v2.1/verify";
 	const options = {
 		method: 'POST',
+		body: 'id_token=' + req.body.userIdToken + '&client_id=' + process.env.LIFF_CHANNEL_ID,
 		headers: {
-			'Content-Type': 'application/x-www-form-urlencoded',
-			'Content-Length': Buffer.byteLength(json_mess)
+			'Content-Type': 'application/x-www-form-urlencoded'
 		}
 	};
 
-	const send_url = "https://api.line.me/oauth2/v2.1/verify?id_token=" + req.body.userIdToken + "&client_id=" + process.env.LIFF_CHANNEL_ID;
-	const client = http.request(send_url, options, (res) => {
-		let resBody = '';
-		res.on('data', (chunk) => { resBody += chunk; });
-		res.on('end', () => {
-			const resObj = JSON.parse(resBody);
-			userId = resObj.sub;
+	// LINE Login の id_token 検証を実行してユーザーIDを取得
+	const response = await fetch(send_url, options);
+	const data = await response.json();
 
-			context.bindings.taxiReserveDocument = JSON.stringify({
-				id: uuid.v4(),
-				userId: userId,
-				userName: req.body.userName,
-				departurePlace: req.body.departurePlace,
-				arrivalPlace: req.body.arrivalPlace,
-				userPhoneNumber: req.body.userPhoneNumber,
-				userNumberOfPassenger: req.body.userNumberOfPassenger,
-				userPassengers: req.body.userPassengers,
-				numberOfTickets: req.body.numberOfTickets,
-				reservationStatus: 1,
-				reservationDatetime: new Date(req.body.reservationDatetime),
-				latestUpdateDatetime: new Date().toISOString()
-			});
+	// Cosmos DB への保存
+	context.bindings.taxiReserveDocument = JSON.stringify({
+		id: uuid.v4(),
+		userId: data.sub,
+		userName: req.body.userName,
+		departurePlace: req.body.departurePlace,
+		arrivalPlace: req.body.arrivalPlace,
+		userPhoneNumber: req.body.userPhoneNumber,
+		userNumberOfPassenger: req.body.userNumberOfPassenger,
+		userPassengers: req.body.userPassengers,
+		numberOfTickets: req.body.numberOfTickets,
+		reservationStatus: 1,
+		reservationDatetime: new Date(req.body.reservationDatetime),
+		latestUpdateDatetime: new Date().toISOString()
+	});
 
-			context.res = {
-				// status defaults to 200 */
-				body: JSON.stringify({
-					userId: userId,
-					status: "success"
-				})
-			};
-			
-			context.done();
-		});
-	});
-	client.on('error', (e) => {
-		context.res = {
-			status: 500,
-			body: resBody + e.stack
-		};
-		context.done();
-	});
-	client.write(json_mess);
-	client.end();
+	context.res = {
+		// status defaults to 200 */
+		body: JSON.stringify({
+			userId: data.sub,
+			status: "success"
+		})
+	};
 };
