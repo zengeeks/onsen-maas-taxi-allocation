8891f5a0b8246d9eb0ba71017862098be3f8e19d
Kazumi OHIRA (@dz_) <dzeyelid@gmail.com>
2021-10-25 09:19:06 +0000

    Add base front-end code by Vite

---
+++ admin/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    '@vue/prettier',
+    '@vue/prettier/@typescript-eslint',
+  ],
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+  },
+  globals: {
+    defineProps: "readonly",
+  }
+}
