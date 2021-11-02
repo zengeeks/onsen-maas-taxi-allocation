8a4b3b14e8a6ad3a2b1a19a15a6a352c1df58305
Kazuyuki Miyake <k-miyake@zengeeks.io>
2021-11-02 22:00:45 +0900

    apply formatter

--- client/api/sendmessage/index.ts
+++ client/api/sendmessage/index.ts
@@ -1,95 +1,102 @@
-import { AzureFunction} from '@azure/functions'
-import fetch from 'node-fetch';
+import { AzureFunction } from '@azure/functions'
+import fetch from 'node-fetch'
 
-const sendmessage: AzureFunction = async function (context, req): Promise<void> {
+const sendmessage: AzureFunction = async function (
+  context,
+  req,
+): Promise<void> {
+  // メッセージ送信用オブジェクトを作成
+  const message = {
+    to: req.body.userId,
+    messages: [
+      {
+        type: 'text',
+        text: req.body.messageText,
+      },
+    ],
+  }
 
-    // メッセージ送信用オブジェクトを作成
-    const message = {
-        to: req.body.userId,
-        messages: [
-            {
-                "type": "text",
-                "text": req.body.messageText
-            }
-        ]
-    }
-
-    // 環境変数から LINE の接続情報を取得
-    const channelId = process.env.LINE_MESSAGING_API_CHANNEL_ID;
-    if (!channelId) {
-        context.res = {
-            status: 500,
-            body: "channelId is not set"
-        };
-        return;
-    }
-    const channelSecret = process.env.LINE_MESSAGING_API_CHANNEL_SECRET;
-    if (!channelSecret) {
-        context.res = {
-            status: 500,
-            body: "channelSecret is not set"
-        };
-        return;
-    }
-
-    // LINE チャネルアクセストークン取得（成功時は channelAccessToken をセット）
-    const channelAccessTokenEndpoint = "https://api.line.me/v2/oauth/accessToken";
-    const channelAccessTokenParams = new URLSearchParams();
-    channelAccessTokenParams.append('grant_type', 'client_credentials');
-    channelAccessTokenParams.append('client_id', channelId);
-    channelAccessTokenParams.append('client_secret', channelSecret);
-    let channelAccessToken = '';
-
-    try {
-        const response = await fetch(channelAccessTokenEndpoint, { method: 'POST', body: channelAccessTokenParams });
-        const data = await response.json();
-        if (!response.ok) {
-            throw new Error(`LINE Access Token API Error: ${data.error} - ${data.error_description}`);
-        }
-        channelAccessToken = data.access_token;
-    } catch (e) {
-        context.log('Error: ', e);
-        if (e instanceof Error) {
-            context.res = {
-                status: 500,
-                body: e.message
-            }
-        }
-        return;
-    }
-
-    // メッセージ送信
-    const messageEndpoint = 'https://api.line.me/v2/bot/message/push';
-    const messageOptions = {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + channelAccessToken
-        },
-        body: JSON.stringify(message)
-    };
-
-    try {
-        const response = await fetch(messageEndpoint, messageOptions);
-        const data = await response.json();
-        if (!response.ok) {
-            throw new Error(`LINE Message API Error: ${data.message}`);
-        }
-    } catch (e) {
-        context.log('Error: ', e);
-        if (e instanceof Error) {
-            context.res = {
-                status: 500,
-                body: e.message
-            }
-        }
-        return;
-    }
-
-    // Function のレスポンスを返す
+  // 環境変数から LINE の接続情報を取得
+  const channelId = process.env.LINE_MESSAGING_API_CHANNEL_ID
+  if (!channelId) {
     context.res = {
-        body: "message sent"
-    };
-};
+      status: 500,
+      body: 'channelId is not set',
+    }
+    return
+  }
+  const channelSecret = process.env.LINE_MESSAGING_API_CHANNEL_SECRET
+  if (!channelSecret) {
+    context.res = {
+      status: 500,
+      body: 'channelSecret is not set',
+    }
+    return
+  }
 
-export default sendmessage;
+  // LINE チャネルアクセストークン取得（成功時は channelAccessToken をセット）
+  const channelAccessTokenEndpoint = 'https://api.line.me/v2/oauth/accessToken'
+  const channelAccessTokenParams = new URLSearchParams()
+  channelAccessTokenParams.append('grant_type', 'client_credentials')
+  channelAccessTokenParams.append('client_id', channelId)
+  channelAccessTokenParams.append('client_secret', channelSecret)
+  let channelAccessToken = ''
+
+  try {
+    const response = await fetch(channelAccessTokenEndpoint, {
+      method: 'POST',
+      body: channelAccessTokenParams,
+    })
+    const data = await response.json()
+    if (!response.ok) {
+      throw new Error(
+        `LINE Access Token API Error: ${data.error} - ${data.error_description}`,
+      )
+    }
+    channelAccessToken = data.access_token
+  } catch (e) {
+    context.log('Error: ', e)
+    if (e instanceof Error) {
+      context.res = {
+        status: 500,
+        body: e.message,
+      }
+    }
+    return
+  }
+
+  // メッセージ送信
+  const messageEndpoint = 'https://api.line.me/v2/bot/message/push'
+  const messageOptions = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: 'Bearer ' + channelAccessToken,
+    },
+    body: JSON.stringify(message),
+  }
+
+  try {
+    const response = await fetch(messageEndpoint, messageOptions)
+    const data = await response.json()
+    if (!response.ok) {
+      throw new Error(`LINE Message API Error: ${data.message}`)
+    }
+  } catch (e) {
+    context.log('Error: ', e)
+    if (e instanceof Error) {
+      context.res = {
+        status: 500,
+        body: e.message,
+      }
+    }
+    return
+  }
+
+  // Function のレスポンスを返す
+  context.res = {
+    body: 'message sent',
+  }
+}
+
+export default sendmessage
--- client/api/taxireserve/index.ts
+++ client/api/taxireserve/index.ts
@@ -1,57 +1,61 @@
 import { AzureFunction } from '@azure/functions'
-import { v4 } from 'uuid';
-import fetch from 'node-fetch';
+import { v4 } from 'uuid'
+import fetch from 'node-fetch'
 
 const taxireserve: AzureFunction = async function (context, req) {
+  let userId = ''
 
-	let userId = "";
+  // LINE Login の id_token 検証（成功時は userID をセット）
+  const idTokenEndpoint = 'https://api.line.me/oauth2/v2.1/verify'
+  const idTokenParams = new URLSearchParams()
+  idTokenParams.append('id_token', req.body.userIdToken)
+  idTokenParams.append('client_id', process.env.LINE_LOGIN_CHANNEL_ID as string)
 
-	// LINE Login の id_token 検証（成功時は userID をセット）
-	const idTokenEndpoint = "https://api.line.me/oauth2/v2.1/verify";
-	const idTokenParams = new URLSearchParams();
-	idTokenParams.append('id_token', req.body.userIdToken);
-	idTokenParams.append('client_id', process.env.LINE_LOGIN_CHANNEL_ID as string);
+  try {
+    const response = await fetch(idTokenEndpoint, {
+      method: 'POST',
+      body: idTokenParams,
+    })
+    const data = await response.json()
+    if (!response.ok) {
+      throw new Error(
+        `LINE ID Token API Error: ${data.error} - ${data.error_description}`,
+      )
+    }
+    userId = data.sub
+  } catch (e) {
+    context.log('Error: ', e)
+    if (e instanceof Error) {
+      context.res = {
+        status: 500,
+        body: e.message,
+      }
+    }
+    return
+  }
 
-	try {
-		const response = await fetch(idTokenEndpoint, { method: 'POST', body: idTokenParams });
-		const data = await response.json();
-		if (!response.ok) {
-			throw new Error(`LINE ID Token API Error: ${data.error} - ${data.error_description}`);
-		}
-		userId = data.sub;
-	} catch (e) {
-		context.log('Error: ', e);
-		if (e instanceof Error) {
-			context.res = {
-				status: 500,
-				body: e.message
-			}
-		}
-		return;
-	}
+  // Cosmos DB への保存
+  context.bindings.taxiReserveDocument = {
+    id: v4(),
+    userId: userId,
+    userName: req.body.userName,
+    departurePlace: req.body.departurePlace,
+    arrivalPlace: req.body.arrivalPlace,
+    userPhoneNumber: req.body.userPhoneNumber,
+    userNumberOfPassenger: req.body.userNumberOfPassenger,
+    userPassengers: req.body.userPassengers,
+    numberOfTickets: req.body.numberOfTickets,
+    reservationStatus: 1,
+    reservationDatetime: new Date(req.body.reservationDatetime),
+    latestUpdateDatetime: new Date().toISOString(),
+  }
 
-	// Cosmos DB への保存
-	context.bindings.taxiReserveDocument = {
-		id: v4(),
-		userId: userId,
-		userName: req.body.userName,
-		departurePlace: req.body.departurePlace,
-		arrivalPlace: req.body.arrivalPlace,
-		userPhoneNumber: req.body.userPhoneNumber,
-		userNumberOfPassenger: req.body.userNumberOfPassenger,
-		userPassengers: req.body.userPassengers,
-		numberOfTickets: req.body.numberOfTickets,
-		reservationStatus: 1,
-		reservationDatetime: new Date(req.body.reservationDatetime),
-		latestUpdateDatetime: new Date().toISOString()
-	};
+  context.res = {
+    body: {
+      userId: userId,
+      status: 'success',
+    },
+  }
+}
 
-	context.res = {
-		body: {
-			userId: userId,
-			status: "success"
-		}
-	};
-};
-
-export default taxireserve;
+export default taxireserve
