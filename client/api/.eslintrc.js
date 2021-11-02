e80c31d1ff0bc0996ef698fdc47ce43d2c27be8a
Kazuyuki Miyake <k-miyake@zengeeks.io>
2021-11-02 22:03:37 +0900

    fix api eslint setting

--- client/api/.eslintrc.js
+++ client/api/.eslintrc.js
@@ -9,7 +9,8 @@ module.exports = {
     {
       parser: '@typescript-eslint/parser',
       parserOptions: {
-        project: './tsconfig.json',
+        project: 'tsconfig.json',
+        tsconfigRootDir: __dirname,
         sourceType: 'module',
       },
       files: ['**/*.ts'],
